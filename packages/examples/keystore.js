d21efd7cb77f2fbecd6515218f991067ad94ba9f
nahee-park <nahee.developer@gmail.com>
2022-10-11 15:11:03 +0900

    lint: eslint에 prettier 설정 추가

--- packages/examples/keystore.js
+++ packages/examples/keystore.js
@@ -1,34 +1,39 @@
-const { JWE } = require("node-jose");
-const { encode } = require("bs58");
-const { randomBytes } = require("crypto");
-const { CHAIN, getAccountFromKeyStore, exportPrivateKey, getAlgo2HashKey } = require("kms/lib");
+const { JWE } = require('node-jose');
+const { encode } = require('bs58');
+const { randomBytes } = require('crypto');
+const { CHAIN, getAccountFromKeyStore, exportPrivateKey, getAlgo2HashKey } = require('kms/lib');
 
-const MNEMONIC = require("./mnemonic.json");
+const MNEMONIC = require('./mnemonic.json');
 
 const LENGTH = 32;
 
 async function createKeyStore(password) {
-  const mnemonic = MNEMONIC.bip44.split(" ");
+  const mnemonic = MNEMONIC.bip44.split(' ');
   const encoder = new TextEncoder();
-  const opt = { t: 2, m: 2048, s: encode(randomBytes(LENGTH)), j: [] };
+  const opt = {
+    t: 2,
+    m: 2048,
+    s: encode(randomBytes(LENGTH)),
+    j: [],
+  };
   const key = await getAlgo2HashKey(password, opt);
-  const jwe = await JWE.createEncrypt({ format: "compact", contentAlg: "A256GCM" }, key)
-    .update(encoder.encode(mnemonic.join(" ")))
+  const jwe = await JWE.createEncrypt({ format: 'compact', contentAlg: 'A256GCM' }, key)
+    .update(encoder.encode(mnemonic.join(' ')))
     .final();
-  return { ...opt, j: jwe.split(".") };
+  return { ...opt, j: jwe.split('.') };
 }
 
 async function getAccountAndPrivagteKey(path, keyStore, password) {
   try {
     const key = await getAlgo2HashKey(password, keyStore);
-    const decryped = await JWE.createDecrypt(key).decrypt(keyStore.j.join("."));
+    const decryped = await JWE.createDecrypt(key).decrypt(keyStore.j.join('.'));
     const account = await getAccountFromKeyStore(path, decryped.plaintext.toString());
     const privateKey = await exportPrivateKey(path, decryped.plaintext.toString());
     return { account, privateKey };
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log(error);
-    return "";
+    return '';
   }
 }
 
@@ -37,18 +42,18 @@ async function run() {
   const accounts = {};
   const keyStore = await createKeyStore(PASSWORD);
   const chains = [
-    ["DSRV", CHAIN.DSRV],
-    ["ETHEREUM", CHAIN.ETHEREUM],
-    ["KLAYTN", CHAIN.KLAYTN],
-    ["CELO", CHAIN.CELO],
-    ["COSMOS", CHAIN.COSMOS],
-    ["NEAR", CHAIN.NEAR],
-    ["SOLANA", CHAIN.SOLANA],
-    ["SUI", CHAIN.SUI],
-    ["TERRA", CHAIN.TERRA],
-    ["PERSISTENCE", CHAIN.PERSISTENCE],
-    ["AGORIC", CHAIN.AGORIC],
-    ["TEZOS", CHAIN.TEZOS],
+    ['DSRV', CHAIN.DSRV],
+    ['ETHEREUM', CHAIN.ETHEREUM],
+    ['KLAYTN', CHAIN.KLAYTN],
+    ['CELO', CHAIN.CELO],
+    ['COSMOS', CHAIN.COSMOS],
+    ['NEAR', CHAIN.NEAR],
+    ['SOLANA', CHAIN.SOLANA],
+    ['SUI', CHAIN.SUI],
+    ['TERRA', CHAIN.TERRA],
+    ['PERSISTENCE', CHAIN.PERSISTENCE],
+    ['AGORIC', CHAIN.AGORIC],
+    ['TEZOS', CHAIN.TEZOS],
     /*
     ["FLOW", CHAIN.FLOW],
     ["POLKADOT", CHAIN.POLKADOT],
