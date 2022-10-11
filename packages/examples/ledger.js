792a470db71dbc44606a436b530d9eeb63164af8
nahee-park <nahee.developer@gmail.com>
2022-10-11 14:41:29 +0900

    docs: edit README.md

--- packages/examples/ledger.js
+++ packages/examples/ledger.js
@@ -1,12 +1,13 @@
-const TransportNodeHid = require("@ledgerhq/hw-transport-node-hid").default;
-const { CHAIN } = require("kms/lib");
-const { getAccount } = require("./ledger/_getAccount");
+const TransportNodeHid = require('@ledgerhq/hw-transport-node-hid').default;
+const { CHAIN } = require('kms/lib');
+const { getAccount } = require('./ledger/_getAccount');
 
 const TYPE = CHAIN.CELO;
 const INDEX = 0;
 
 async function run() {
   const transport = await TransportNodeHid.create(1000);
+  console.log('account', transport, TYPE, INDEX);
   await getAccount(transport, TYPE, INDEX);
   transport.close();
 }
