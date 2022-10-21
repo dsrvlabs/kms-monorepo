e7ec7b8ee31c4097928286eade84a9fe63b92343
nahee-park <nahee.developer@gmail.com>
2022-10-21 20:17:28 +0900

    WEL-17 feat: create CeloSignedTx with sdk

--- packages/examples/sdkSignPlayground.ts
+++ packages/examples/sdkSignPlayground.ts
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
+import { celoSdkSignedTx } from './utils/sdkSignedTx/celoSdkSignedTx';
 import { ethereumSdkSignedTx } from './utils/sdkSignedTx/ethereumSdkSignedTx';
 
 const MNEMONIC = require('./mnemonic.json');
@@ -7,6 +9,9 @@ const main = async () => {
   const mnemonic = MNEMONIC.bip44;
   const ethereumSingedTx = await ethereumSdkSignedTx(mnemonic);
   console.log('[By SDK] Ethereum Tx : ', ethereumSingedTx);
+
+  const celoSignedTx = await celoSdkSignedTx(mnemonic);
+  console.log('[By SDK] Celo Tx : ', celoSignedTx);
 };
 
 main();
